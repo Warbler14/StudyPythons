04966f2f8fb8d1a268a2fccfa0f6bd69d5cdc4f6
Warbler14 <firefly11a@naver.com>
2021-12-21 01:26:24 +0900

    study

---
+++ works/practice/300/011-020.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+samsung = 50000
+stock_count = 10
+print('price', samsung * stock_count)
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+s = "hello"
+t = "python"
+print(s + '!', t)
+print(2 + 2 * 3)
+
+a = "132"
+print(type(a))
+
+num_str = "720"
+num_int = int(num_str)
+print(num_int, type(num_int))
+
+num = 100
+num_as_str = str(num)
+print(num_as_str, type(num_as_str))
+
+num_float = float("15.29")
+print(num_float, type(num_float))
+
+year = "2021"
+year_int = int(year)
+print(year_int - 3, year_int - 2, year_int - 1)
+
+interest_month = 36
+price_per_month = 48584
+print('total price :', price_per_month * interest_month)
---
+++ works/practice/300/021-030.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+letters = 'python'
+print(letters[0], letters[2])
+
+license_plate = "24가 2210"
+print(license_plate[4:])
+
+string = "홀짝홀짝홀짝"
+print(string[0] + string[2] + string[4])
+print(string[::2])
+
+string = "PYTHON"
+print(string[::-1])
+
+phone_number = "010-1111-2222"
+print(phone_number.replace("-", " "))
+
+print(phone_number.replace("-", ""))
+
+url = "http://sharebook.kr"
+print(url[url.index('.') + 1:])
+url_split = url.split('.')
+print(url_split[-1])
+
+'''
+lang = 'python'
+lang[0] = 'P'   # TypeError: 'str' object does not support item assignment
+print(lang)
+'''
+
+string = 'abcdfe2a354a32a'
+print(string.replace('a', 'A'))
+
+string = 'abcd'
+string.replace('b', 'B')
+print(string)   # abcd
