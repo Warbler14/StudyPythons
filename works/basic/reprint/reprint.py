0e8bb9e165f68efff17aaef0bc1d555cc7388e95
kook <insungkook@gmail.com>
2024-08-28 00:28:41 +0900

    'TERM environment variable not set.' error fixed

--- works/basic/reprint/reprint.py
+++ works/basic/reprint/reprint.py
@@ -1,18 +1,21 @@
 import time
 import os
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+# ! enable Emulate terminal in output console option for this run configuration
+
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')  # For Windows
+    else:
+        os.environ['TERM'] = 'xterm'  # Set TERM to 'xterm' for other platforms like macOS and Linux
+        os.system('clear')
 
 
 message = "Hello World"
-
 print(message)
 
 time.sleep(1)
-
-os.system('clear')
+clear_screen()
 
 message = "Goodbye World"
-
 print(message)
